973ccae0f47fd22c5c9b4ba01852b8d55bc7c68b
ismoil3 <sufonkulzodaismoil@gmail.com>
2024-07-20 09:31:19 +0500

    app.js

---
+++ app.js
@@ -0,0 +1,59 @@
+// function asd(a){
+//     let cnt = 0;
+//      a.map((e)=>{
+//     cnt+=e
+//     })
+//     return cnt
+// }
+// console.log(asd([2,4,6,8]));
+
+
+
+
+// taks 2
+// function asd(a){
+// return Math.max(...a)
+// }
+// console.log(asd([2,4,6,8]));
+
+
+
+
+// function asd(a){
+//     let cnt = 0;
+//     a.map((e)=>{
+//        if(e%2==0){
+//     cnt++
+//        }
+//     })
+//     return cnt
+// }
+// console.log(asd([1,2,3,4,5,6]));
+
+
+
+// function asd(a,b){
+//   for(let i=0;i<a.length;i++){
+//     if(a[i]===b){
+//       return true;
+//     }
+//   }
+//   return false;
+// }
+// console.log(asd([10,20,30,40],30));
+
+
+
+
+
+
+// function arithmetic(arr){
+//     let sum = 0;
+//     let cnt=0
+//     arr.map((e)=>{
+//     cnt++
+//     sum+=e
+//     })
+//     return sum/cnt
+// }
+// console.log(arithmetic([2,4,6,8]));
